ad4188bee113f4e367002143b4afbf05108bd989
Plyschbjörn <plysch@proton.me>
2025-09-08 12:46:46 +0200

    changed marts sql

--- dbt/dbt_worksheets/setup_marts_layer.sql
+++ dbt/dbt_worksheets/setup_marts_layer.sql
@@ -1,38 +1,39 @@
--- We use marts layers to serve the data 
+-- We use marts layers to serve the data
 
 USE ROLE SYSADMIN;
 
-USE DATABASE job_ads;   
+USE DATABASE project_hr;
 
 CREATE SCHEMA IF NOT EXISTS marts;
 
-SHOW SCHEMAS IN DATABASE job_ads;
+SHOW SCHEMAS IN DATABASE project_hr;
 
 USE ROLE securityadmin;
 
 GRANT USAGE,
 CREATE TABLE,
-CREATE VIEW ON SCHEMA job_ads.marts TO ROLE job_ads_dbt_role;
+CREATE VIEW ON SCHEMA project_hr.marts TO ROLE dbt_dev_role;
 
 -- grant CRUD and select tables and views
 GRANT SELECT,
 INSERT,
 UPDATE,
-DELETE ON ALL TABLES IN SCHEMA job_ads.marts TO ROLE job_ads_dbt_role;
-GRANT SELECT ON ALL VIEWS IN SCHEMA job_ads.marts TO ROLE job_ads_dbt_role;
+DELETE ON ALL TABLES IN SCHEMA project_hr.marts TO ROLE dbt_dev_role;
+GRANT SELECT ON ALL VIEWS IN SCHEMA project_hr.marts TO ROLE dbt_dev_role;
 
 -- grant CRUD and select on future tables and views
 GRANT SELECT,
 INSERT,
 UPDATE,
-DELETE ON FUTURE TABLES IN SCHEMA job_ads.marts TO ROLE job_ads_dbt_role;
-GRANT SELECT ON FUTURE VIEWS IN SCHEMA job_ads.marts TO ROLE job_ads_dbt_role;
-USE ROLE job_ads_dbt_role;
+DELETE ON FUTURE TABLES IN SCHEMA project_hr.marts TO ROLE dbt_dev_role;
+GRANT SELECT ON FUTURE VIEWS IN SCHEMA project_hr.marts TO ROLE dbt_dev_role;
 
-SHOW GRANTS ON SCHEMA job_ads.marts;
+USE ROLE dbt_dev_role;
+
+SHOW GRANTS ON SCHEMA project_hr.marts;
 
 -- manual test
-USE SCHEMA job_ads.marts;
+USE SCHEMA project_hr.marts;
 CREATE TABLE test (id INTEGER);
 SHOW TABLES;
 DROP TABLE TEST;
